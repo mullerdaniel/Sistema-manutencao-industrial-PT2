194fb2f7b21a7f5d49ca7beef28fb024dcbe8157
Daniel Müller <miladaniel370@gmail.com>
2025-10-16 18:47:35 -0300

    DM-1 feat: Fazendo conexão

---
+++ src/main/java/org/example/Conexao.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/manutencao_industrial?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String SENHA = "mysqlPW";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, SENHA);
+    }
+}
